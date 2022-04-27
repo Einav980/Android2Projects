7665b17a2fec4e8b87c0a1c91c11b51edf24e3eb
einav malcka <einav980@gmail.com>
2022-04-27 23:29:16 +0300

    changes to apartment table

--- Rently/server/models/apartment.model.js
+++ Rently/server/models/apartment.model.js
@@ -4,6 +4,14 @@ const apartmentModel = mongoose.Schema({
   city: { type: String, require: true },
   numberOfRooms: { type: Number, require: true },
   price: { type: Number, require: true },
+  numberOfBaths: {type: Number, require: false}, 
+  address: { type: String, require: true },
+  lat: { type: Number, require: true },
+  lng: { type: Number, require: true },
+  size: { type: Number, require: true },
+  type: { type: String, require: true },
+  imageUrl: { type: String, require: true },
+  status: {type: String , require: true}
 });
 
 module.exports = mongoose.model('apartments', apartmentModel, 'apartments');
