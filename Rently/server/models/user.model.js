66c547b9d7abfa11ee975ccf862b5c695497b4dc
einav malcka <einav980@gmail.com>
2022-06-12 00:55:24 +0300

    get user API change in server side

--- Rently/server/models/user.model.js
+++ Rently/server/models/user.model.js
@@ -2,8 +2,11 @@ const { default: mongoose } = require('mongoose');
 
 const userModel = mongoose.Schema({
   email: { type: String, require: true },
+  lastname: { type: String, require: true },
+  firstname: { type: String, require: true },
   hashedPassword: { type: String, require: true },
   phone: { type: String, require: true },
+  type: { type: String, require: true },
 });
 
 module.exports = mongoose.model('User', userModel, 'users');
--- Rently/server/router/user.router.js
+++ Rently/server/router/user.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/users', userController.listUsers);
-router.get('/users/:id', userController.getUser);
+router.get('/users/:email', userController.getUser);
 router.post('/users/signup', userController.signUpUser);
 router.post('/users/login', userController.loginUser);
 
