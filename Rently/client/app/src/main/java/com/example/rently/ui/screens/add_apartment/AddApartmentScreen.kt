484ae24f36617246bbf6fc974fd732689acf4417
kokolight <tsegal97@gmail.com>
2022-06-14 01:22:23 +0300

    added add apartment page

--- Rently/client/app/src/main/java/com/example/rently/ui/screens/add_apartment/AddApartmentScreen.kt
+++ Rently/client/app/src/main/java/com/example/rently/ui/screens/add_apartment/AddApartmentScreen.kt
@@ -1,29 +1,37 @@
 package com.example.rently.ui.screens.add_apartment
 
-import android.util.Log
+import android.graphics.Bitmap
+import android.net.Uri
+import android.util.Base64
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.rently.navigation.Screen
-import com.example.rently.ui.components.ApartmentCard
+import com.example.rently.ui.components.Counter
 import com.example.rently.ui.theme.RentlyTheme
+import java.io.ByteArrayOutputStream
+
 
 @Composable
 fun AddApartmentScreen(viewModel: AddApartmentViewModel = hiltViewModel()) {
@@ -33,13 +41,54 @@ fun AddApartmentScreen(viewModel: AddApartmentViewModel = hiltViewModel()) {
 
     val context = LocalContext.current
 
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
+    var imageEncodedString by remember {
+        mutableStateOf("")
+    }
+
+    val bitmap = remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+
+    var counter by remember {
+        mutableStateOf(0)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract =
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+        if (imageUri != null) {
+            bitmap.value = viewModel.getImageBitmap(context = context, imageUri = imageUri!!)
+            imageEncodedString = convertImageToBase64(bitmap = bitmap.value!!)
+            if (imageEncodedString.isNotEmpty()) {
+                viewModel.uploadImage(imageEncodedString)
+            }
+        }
+    }
+
+
     RentlyTheme {
         Column(
-            modifier = Modifier.fillMaxSize().padding(10.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(modifier = Modifier.fillMaxWidth().padding(10.dp), text = "Add apartment", style = MaterialTheme.typography.h5)
-            Column(modifier = Modifier.width(350.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp), text = "Add apartment", style = MaterialTheme.typography.h5
+            )
+            Column(
+                modifier = Modifier.width(350.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
                 TextField(
                     modifier = Modifier.fillMaxWidth(),
                     value = addressInput,
@@ -58,9 +107,9 @@ fun AddApartmentScreen(viewModel: AddApartmentViewModel = hiltViewModel()) {
                 if (viewModel.isLoading.value) {
                     CircularProgressIndicator()
                 }
-                if(viewModel.showPredictions.value){
-                    LazyColumn(Modifier.border(1.dp, Color.Black)){
-                        items(items = viewModel.predictions.value) { prediction ->
+                if (viewModel.showPredictions.value) {
+                    LazyColumn(Modifier.border(1.dp, Color.Black)) {
+                        items(items = viewModel.predictions) { prediction ->
                             Surface(
                                 modifier = Modifier
                                     .clickable {
@@ -73,12 +122,35 @@ fun AddApartmentScreen(viewModel: AddApartmentViewModel = hiltViewModel()) {
                                     .fillMaxWidth()
                                     .background(MaterialTheme.colors.background)
                             ) {
-                                Text(text = prediction.description, modifier = Modifier.padding(10.dp))
+                                Text(
+                                    text = prediction.description,
+                                    modifier = Modifier.padding(10.dp)
+                                )
                             }
                         }
                     }
                 }
             }
+
+            Button(modifier = Modifier.size(200.dp), onClick = { launcher.launch("image/*") }) {
+                if (imageUri != null) {
+                    Image(
+                        bitmap = bitmap.value!!.asImageBitmap(),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clickable {
+
+                            },
+                        contentScale = ContentScale.Crop,
+                    )
+                } else {
+                    ImagePlaceHolder()
+                }
+            }
+
+            Counter(value = counter, onValueIncrease = {counter++}, onValueDecrease = {counter--}, size = 30.dp, fontSize = 24.sp, label = "Bedrooms")
+            Counter(value = counter, onValueIncrease = {counter++}, onValueDecrease = {counter--}, size = 30.dp, fontSize = 24.sp, label = "Toilets")
         }
     }
 }
@@ -87,4 +159,27 @@ fun AddApartmentScreen(viewModel: AddApartmentViewModel = hiltViewModel()) {
 @Composable
 fun AddApartmentScreenPreview() {
     AddApartmentScreen()
+}
+
+fun convertImageToBase64(bitmap: Bitmap): String {
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+    val byteArray = byteArrayOutputStream.toByteArray()
+    val encodedImage =
+        try {
+            Base64.encodeToString(byteArray, Base64.DEFAULT);
+        } catch (exception: Exception) {
+            ""
+        }
+
+    return encodedImage
+}
+
+@Composable
+fun ImagePlaceHolder() {
+    Image(
+        imageVector = Icons.Filled.Image,
+        contentDescription = "Image",
+        modifier = Modifier.fillMaxSize()
+    )
 }
--- Rently/client/app/src/main/java/com/example/rently/ui/screens/add_apartment/AddApartmentViewModel.kt
+++ Rently/client/app/src/main/java/com/example/rently/ui/screens/add_apartment/AddApartmentViewModel.kt
@@ -1,35 +1,51 @@
 package com.example.rently.ui.screens.add_apartment
 
+import android.content.Context
 import android.gesture.Prediction
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.text.method.TextKeyListener.clear
+import android.util.Base64
 import android.util.Log
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.rently.Resource
 import com.example.rently.model.Apartment
 import com.example.rently.model.GooglePrediction
 import com.example.rently.repository.GooglePlacesRepository
+import com.example.rently.repository.ImagesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.io.ByteArrayOutputStream
 import javax.inject.Inject
 
 @HiltViewModel
-class AddApartmentViewModel @Inject constructor(private val googleRepository: GooglePlacesRepository): ViewModel(){
+class AddApartmentViewModel @Inject constructor(
+    private val googleRepository: GooglePlacesRepository,
+    private val imagesRepository: ImagesRepository
+) : ViewModel() {
 
     val isLoading = mutableStateOf(false)
     val showPredictions = mutableStateOf(false)
-    val predictions = mutableStateOf(ArrayList<GooglePrediction>())
+    var predictions = mutableStateListOf<GooglePrediction>()
 
     private fun getPredictions(address: String) {
         showPredictions.value = true
         viewModelScope.launch {
             isLoading.value = true
             val response = googleRepository.getPredictions(input = address)
-            when(response){
+            when (response) {
                 is Resource.Success -> {
-                    predictions.value = response.data?.predictions!!
+                    predictions = response?.data?.predictions!!.toMutableStateList()
                 }
             }
 
@@ -37,16 +53,62 @@ class AddApartmentViewModel @Inject constructor(private val googleRepository: Go
         }
     }
 
-    fun showPredictions(){
+    fun showPredictions() {
         showPredictions.value = true
     }
 
-    fun hidePredictions(){
+    fun hidePredictions() {
         showPredictions.value = false
     }
 
-    fun onSearchAddressChange(address: String){
+    fun onSearchAddressChange(address: String) {
         getPredictions(address)
         showPredictions()
+    }
+
+    // Todo
+    fun uploadMultipleImages() {
+
+    }
+
+    fun uploadImage(imageBase64: String) {
+        viewModelScope.launch {
+            val response = imagesRepository.uploadImage(image = imageBase64)
+            when (response) {
+                is Resource.Success -> {
+                    Log.d("Rently", "${response.data?.data?.url}")
+                    Log.d("Rently", "Uploaded successfully!")
+                }
+                else -> {
+                    Log.d("Rently", "Error while uploading image")
+                }
+            }
+        }
+    }
+
+    private fun convertImageToBase64(bitmap: Bitmap): String {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val byteArray = byteArrayOutputStream.toByteArray()
+        val encodedImage =
+            try {
+                Base64.encodeToString(byteArray, Base64.DEFAULT);
+            } catch (exception: Exception) {
+                ""
+            }
+
+        return encodedImage
+    }
+
+    fun getImageBitmap(context: Context, imageUri: Uri): Bitmap {
+        return if (Build.VERSION.SDK_INT < 28) {
+            MediaStore.Images
+                .Media.getBitmap(context.contentResolver, imageUri)
+
+        } else {
+            val source = ImageDecoder
+                .createSource(context.contentResolver, imageUri!!)
+            ImageDecoder.decodeBitmap(source)
+        }
     }
 }
