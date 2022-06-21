94122fc4ce55fa8a5c4e6701126045907584f39c
kokolight <tsegal97@gmail.com>
2022-06-22 01:47:09 +0300

    updated counter component

--- Rently/client/app/src/main/java/com/example/rently/ui/components/Counter.kt
+++ Rently/client/app/src/main/java/com/example/rently/ui/components/Counter.kt
@@ -11,75 +11,56 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Bed
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.rently.ui.theme.RentlyTheme
 
 @Composable
 fun Counter(
     value: Int,
-    onValueIncrease: (Int) -> Unit,
-    onValueDecrease: (Int) -> Unit,
-    size: Dp = 15.dp,
+    onValueChanged: (Int) -> Unit,
     backgroundColor: Color = MaterialTheme.colors.primary,
     tint: Color = Color.White,
-    fontSize: TextUnit = 18.sp,
-    label: String = "Label",
-    labelSize: TextUnit = 28.sp
+    icon: ImageVector? = null,
+    fontStyle: TextStyle = MaterialTheme.typography.h4,
 ) {
     Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = label, fontSize = labelSize, textAlign = TextAlign.Center)
-        Spacer(modifier = Modifier.height(5.dp))
-        Box(modifier = Modifier.size(40.dp).clip(RoundedCornerShape(10.dp)).background(backgroundColor), contentAlignment = Alignment.Center){
-            Text(modifier = Modifier.padding(5.dp), text = value.toString(), fontSize = fontSize)
+        Text(text = value.toString(), style = fontStyle, fontWeight = FontWeight.Bold)
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween){
+            IconButton(modifier = Modifier.clip(CircleShape).background(backgroundColor).size(32.dp), onClick = {onValueChanged(value + 1)}) {
+                Icon(modifier = Modifier.padding(3.dp), imageVector = Icons.Filled.Add, contentDescription = "Add", tint = tint)
+            }
+            if(icon != null){
+                Box(modifier = Modifier.padding(5.dp)){
+                    Icon(modifier = Modifier.padding(8.dp).scale(1.5f), imageVector = icon, contentDescription = null)
+                }
+            }
+            IconButton(modifier = Modifier.clip(CircleShape).background(backgroundColor).size(32.dp), onClick = {onValueChanged(value - 1)}) {
+                Icon(modifier = Modifier.padding(3.dp), imageVector = Icons.Filled.Remove, contentDescription = "Remove", tint = tint)
+            }
         }
-        /*Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            IconButton(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(backgroundColor)
-                    .size(size),
-                onClick = { onValueIncrease(value) },
-                content = {
-                    Icon(
-                        imageVector = Icons.Filled.Add, contentDescription = "Add", tint = tint
-                    )
-                },
-            )
-
-            IconButton(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(backgroundColor)
-                    .size(size)
-                    .padding(5.dp),
-                onClick = { onValueDecrease(value) },
-                content = {
-                    Icon(
-                        imageVector = Icons.Filled.Remove, contentDescription = "Remove", tint = tint
-                    )
-                },
-            )
-        }*/
     }
 }
 
 @Preview
 @Composable
 fun CounterPreview() {
-    MaterialTheme() {
-        Counter(value = 3, onValueIncrease = {}, onValueDecrease = {})
+    RentlyTheme() {
+        Counter(value = 3, onValueChanged = {}, icon = Icons.Filled.Bed)
     }
 }
