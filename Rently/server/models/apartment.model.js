b41fed1c0baf9e0d7048d1b7d3d34a0fd9764d65
kokolight <tsegal97@gmail.com>
2022-06-23 01:13:16 +0300

    updated apartment model

--- Rently/server/models/apartment.model.js
+++ Rently/server/models/apartment.model.js
@@ -1,18 +1,21 @@
 const { default: mongoose } = require('mongoose');
 
 const apartmentModel = mongoose.Schema({
+  userId: { type: String, require: true },
   city: { type: String, require: true },
-  numberOfRooms: { type: Number, require: true },
-  price: { type: Number, require: true },
+  numberOfBeds: { type: Number, require: true },
   numberOfBaths: { type: Number, require: false },
+  price: { type: Number, require: true },
   address: { type: String, require: true },
-  lat: { type: Number, require: true },
-  lng: { type: Number, require: true },
+  Location: { type: { lat: Number, lng: Number }, require: true },
   size: { type: Number, require: true },
   type: { type: String, require: true },
   imageUrl: { type: String, require: true },
   status: { type: String, require: true },
-  userId: { type: String, require: true },
+  hasBalcony: { type: Boolean, require: true },
+  hasParking: { type: Boolean, require: true },
+  isFurnished: { type: Boolean, require: true },
+  isAnimalFriendly: { type: Boolean, require: true },
 });
 
 module.exports = mongoose.model('apartment', apartmentModel, 'apartments');
