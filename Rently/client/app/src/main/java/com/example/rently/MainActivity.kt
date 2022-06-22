147b55d74ae143104a4ecd729760219bdf4afa7c
kokolight <tsegal97@gmail.com>
2022-06-23 01:01:06 +0300

    deleted unnecessary lines

--- Rently/client/app/src/main/java/com/example/rently/MainActivity.kt
+++ Rently/client/app/src/main/java/com/example/rently/MainActivity.kt
@@ -1,27 +1,15 @@
 package com.example.rently
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.preferencesDataStore
-import androidx.lifecycle.coroutineScope
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
 import com.example.rently.navigation.SetupNavGraph
-import com.example.rently.repository.datastore
 import com.example.rently.ui.theme.RentlyTheme
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
