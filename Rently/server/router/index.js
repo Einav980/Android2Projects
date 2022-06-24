c60eda7348a9bc87f7927047e2bee55b813e6943
einav malcka <einav980@gmail.com>
2022-06-24 11:48:18 +0300

    changes to server watchlist

--- Rently/server/router/index.js
+++ Rently/server/router/index.js
@@ -1,7 +1,8 @@
 const apartmentRouter = require('./apartment.router');
 const userRouter = require('./user.router');
 const apartmentTypeRouter = require('./apartmentType.router');
+const watchlistRouter = require('./watchlist.router');
 
-const routers = { apartmentRouter, userRouter, apartmentTypeRouter };
+const routers = { apartmentRouter, userRouter, apartmentTypeRouter, watchlistRouter };
 
 module.exports = routers;
