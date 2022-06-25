383532dd232b3e43a90697b24f3237f2f2b6ca81
kokolight <tsegal97@gmail.com>
2022-06-25 16:15:39 +0300

    changed directory structure

--- Rently/client/app/src/main/java/com/example/rently/validation/use_case/ApartmentsScreenState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.rently.validation.use_case
-
-import com.example.rently.model.Apartment
-
-data class ApartmentsScreenState(
-    val apartments: List<Apartment> = emptyList(),
-    val loading: Boolean = false,
-)
